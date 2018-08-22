52f23a1b2116aae1e772c2fb5a982d3bd20e0875
Jerome Fitzgerald <jeromes@gmail.com>
2018-08-22 18:24:29 -0400

    🍻️ Okay, just stop. This is not even going to get merged.

--- jest.config.js
+++ jest.config.js
@@ -2,16 +2,21 @@ module.exports = {
   collectCoverageFrom: ['<rootDir>/src/**/*.js'],
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'html'],
+  moduleFileExtensions: ['', 'js', 'json'],
   setupFiles: ['<rootDir>/jest.setup.js'],
   setupTestFrameworkScriptFile: './node_modules/jest-enzyme/lib/index.js',
+  // setupTestFrameworkScriptFile: 'jest-enzyme',
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  testEnvironment: 'enzyme',
+  // testEnvironmentOptions: {
+  //   enzymeAdapter: 'react16'
+  // },
   testMatch: [
     '<rootDir>/src/__tests__/**/*.js?(x)',
     '<rootDir>/src/**/?(*-)(spec|test).js?(x)'
   ],
-  testURL: 'http://localhost',
   testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+  testURL: 'http://localhost',
   transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
-  moduleFileExtensions: ['', 'js', 'json'],
-  snapshotSerializers: ['enzyme-to-json/serializer'],
-  unmockedModulePathPatterns: ['react', 'enzyme', 'jest-enzyme']
+  unmockedModulePathPatterns: ['react', 'enzyme', 'jest-enzyme'],
 }
